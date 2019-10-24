ae1b2dee932b435b3ba78f62ce5bdf9a52532410
David Lin <david@Davids-MacBook-Pro.local>
2019-10-24 10:40:52 -0400

    testing commits

---
+++ BlackJack/BlackJack/Suit.swift
@@ -0,0 +1,14 @@
+//
+//  Suit.swift
+//  BlackJack
+//
+//  Created by David Lin on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+enum Suit: String {
+    case diamond = "♦️"
+    case
+}
